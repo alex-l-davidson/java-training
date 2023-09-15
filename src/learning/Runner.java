4e24565659671326d184877192b1dc929d9670d5
alex-l-davidson <alexandra.davidson@sky.uk>
2023-09-15 16:37:58 +0100

    added variable types exercise to Runner.java

--- src/learning/Runner.java
+++ src/learning/Runner.java
@@ -3,5 +3,20 @@ package learning;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
     }
+    /*
+    byte sizeof = 127;
+    short mum = 43;
+    short hello mum;
+    int big = sizeof * sizeof * sizeof;
+    long bigger = big + big + big;
+    double old = 78.0;
+    double new = 0.1;
+    boolean consequence = true;
+    boolean a, b; c;
+    boolean max = big > bigger;
+    char maine = 'american state';
+    char ming = 'd';
+     */
 }
